f5b32e5ef88fe8789868b273822d2238a8a39095
zero <emservi30@gmail.com>
2025-07-13 15:53:16 +0530

    add header like figma , update css for header

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -7,10 +8,28 @@
   <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="bootstrap.css" />
 </head>
-<body>
-  
-  
 
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="g-0">
+        <img src="resource/main_banner.jpg" alt="body_banner" class="img-fluid" style="height: 1024;" />
+      </div>
+      <div class="offset-md-1 col-md-10 header d-flex" style="position: absolute;">
+        <div class="col-md-3 align-items-center">
+          <p style="font-size: 46px ; margin-top: 5px; white-space: nowrap;"><span style="color: #D54545;">K</span>avindu <span style="color: #D54545;">D</span>ev</p>
+        </div>
+        <div class="col-md-6 d-flex" style="gap: 65px; font-size: 18px; font-weight: 700; justify-content: center; align-items: center;">
+          <span>Home</span>
+          <span>About</span>
+          <span>Services</span>
+          <span>Portfolio</span>
+          <span>Contact</span>
+        </div>
+        <div class="offset-md-1 col-md-2 d-flex justify-content-center align-items-end"><p class="header-button">Let’s Talk</p></div>
+      </div>
+    </div>
+  </div>
   <script src="script.js"></script>
   <script src="bootstrap.js"></script>
 </body>
