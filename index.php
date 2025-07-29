771d8d2647fa166d822fa885639f3fcee5ea7d43
zero <emservi30@gmail.com>
2025-07-29 18:49:20 +0530

    Add mobile view layout and styles for main

--- index.php
+++ index.php
@@ -7,37 +7,52 @@
   <title>My Basic Page</title>
   <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="bootstrap.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 
 <body>
   <div class="container-fluid">
     <div class="row">
-      <div class="g-0">
-        <img src="resource/main_banner.jpg" alt="body_banner" class="img-fluid" style="height: 1024;" />
+      <!-- Desktop Background -->
+      <div class="g-0 d-none d-md-block">
+        <img src="resource/main_banner.jpg" alt="body_banner" class="img-fluid main-image" style="height: 100vh; width: 100%" />
       </div>
-      <div class="offset-md-1 col-md-10 header d-flex" style="position: absolute;">
-        <div class="col-md-3 align-items-center">
+
+      <!-- Mobile background -->
+      <div class="mobile-main-background col-12 d-block d-md-none">
+        <div class="mobile-background-circle"></div>
+        <div class="menu-bar">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </div>
+
+      <div class="offset-md-1 col-md-10 header d-none d-md-flex">
+        <div class="col-md-3 align-items-center header-text">
           <p style="font-size: 46px ; margin-top: 5px; white-space: nowrap;"><span style="color: #D54545;">K</span>avindu <span style="color: #D54545;">D</span>ev</p>
         </div>
-        <div class="col-md-6 d-flex" style="gap: 65px; font-size: 18px; font-weight: 700; justify-content: center; align-items: center;">
-          <span>Home</span>
-          <span>About</span>
-          <span>Services</span>
-          <span>Portfolio</span>
-          <span>Contact</span>
+        <div class="col-md-6 d-flex header-nav" style="gap: 65px; font-size: 18px; font-weight: 700; justify-content: center; align-items: center;">
+          <span class="header-text">Home</span>
+          <span class="header-text">About</span>
+          <span class="header-text">Services</span>
+          <span class="header-text">Portfolio</span>
+          <span class="header-text">Contact</span>
         </div>
         <div class="offset-md-1 col-md-2 d-flex justify-content-center align-items-end">
           <p class="header-button">Let’s Talk</p>
         </div>
       </div>
-      <div class="main-content-group offset-md-1 col-md-5 g-0 position-absolute" style="top: 40%;">
-        <p class="main-hi">Hi ,i’m</p>
-        <p class="main-name main-gap">Kavindu</p>
+      <div class="main-content-group offset-1 col-10 col-md-5 g-0">
+        <p class="main-hi d-none d-md-block">Hi ,i’m</p>
+        <p class="main-hi d-block d-md-none">Hi ,i’m Kavindu</p>
+        <p class="main-name main-gap d-none d-md-block">Kavindu</p>
         <p class="main-role main-gap">I,m <span class="main-role-highlight">d</span>eveloper & <span class="main-role-highlight">P</span>rogrammer</p>
         <p class="main-desc main-gap-desc">I am a visionary thinker with strong leadership, creativity, and communication skills. Passionate about innovation and problem-solving, I thrive in dynamic environments, bringing ideas to life through design and technology.</p>
-      </div>
-      <div class="g-0 position-absolute offset-md-1 col-md-2 d-flex justify-content-center align-items-center sty">
-        <button class="main-button">Contact Us</button>
+        <div class="offset-3 col-6">
+          <button class="mobile-main-button d-md-none">Contact Us</button>
+        </div>
+        <div class="col-md-5">
+          <button class="main-button d-none d-md-flex">Contact Us</button>
+        </div>
       </div>
     </div>
   </div>
